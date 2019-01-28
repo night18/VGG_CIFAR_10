85311d8e46b1733d5c2d5963ee1d17f3471b99ee
night18 <warrior.sp@gmail.com>
2019-01-28 12:52:07 -0500

    send to AWS

--- vgg_test.py
+++ vgg_test.py
@@ -8,7 +8,9 @@ import vgg_train
 import matplotlib.pyplot as plt
 
 cifar_10_dir = "cifar-10"
-epochs = 30
+epochs = 13
+validation_number = 5000
+train_number = 50000 - validation_number
 
 def testModel(model, model_name, x_test, y_test):
 	score = model.evaluate(x_test, y_test)
@@ -29,7 +31,8 @@ def plot_performance(histories, name_list, isloss = True):
 	plt.ylabel(perforemance)
 	plt.xlabel( "epochs" )
 	plt.legend( name_list , loc=0)
-	plt.show()
+	# plt.show()
+	plt.savefig("learning_rate.png")
 
 
 
@@ -38,10 +41,11 @@ if __name__ == '__main__':
 	#load data set 
 	train_data, _, train_labels, test_data, _, test_labels, label_names = util.loadCIFAR10(cifar_10_dir)
 
-	train_data, validation_data = train_data[0:800], train_data[800:1000]
-	train_labels, validation_labels = train_labels[0:800], train_labels[800:1000]
-	test_data =  test_data[0:1000]
-	test_labels = test_labels[0:1000]
+
+	train_data, validation_data = train_data[ 0:train_number ], train_data[ train_number:50000 ]
+	train_labels, validation_labels = train_labels[ 0:train_number ], train_labels[ train_number:50000 ]
+	# test_data =  test_data[0:1000]
+	# test_labels = test_labels[0:1000]
 
 
 	train_data, validation_data, test_data = train_data/255.0, validation_data/255.0 , test_data/255.0
@@ -50,17 +54,28 @@ if __name__ == '__main__':
 	test_labels= to_categorical(test_labels,num_classes=10)
 
 	learning_rate_list = [1, 0.1, 0.01, 0.001, 0.0001]
+	model_list = ["vgg_11", "vgg_11_02", "vgg_11_03"]
 	histories = []
 	name_list = []
 
-	for x in learning_rate_list:
-		model = vgg_train.loadModel("vgg_11", x)
-		if model == None:
-			model = vgg_train.trainModel("vgg_11", train_data, train_labels, validation_data, validation_labels, epochs = 2, learning_rate = x)
-
-		testModel(model, "vgg_11_"+str(x), test_data, test_labels )
-
-		path = "history/vgg_11_{}".format(x)
+	for x in model_list:
+		model = vgg_train.loadModel(x)
+		if model == None: 
+			model = vgg_train.trainModel(x, train_data, train_labels, validation_data, validation_labels, epochs = epochs)
+		testModel(model, x, test_data, test_labels )
+		path = "history/{}_0.01".format(x)
 		histories.append( util.unpickle(path) )
 
-	plot_performance(histories, learning_rate_list, isloss = False)
+	plot_performance(histories, model_list, isloss = False)
+	
+	# for x in learning_rate_list:
+	# 	model = vgg_train.loadModel("vgg_11", x)
+	# 	if model == None:
+	# 		model = vgg_train.trainModel("vgg_11", train_data, train_labels, validation_data, validation_labels, epochs = epochs, learning_rate = x)
+
+	# 	testModel(model, "vgg_11_"+str(x), test_data, test_labels )
+
+	# 	path = "history/vgg_11_{}".format(x)
+	# 	histories.append( util.unpickle(path) )
+
+	# plot_performance(histories, learning_rate_list, isloss = False)
--- vgg_train.py
+++ vgg_train.py
@@ -7,7 +7,7 @@ Description: Train VGG-like network
 '''
 import tensorflow as tf
 from tensorflow.keras.models import Sequential, save_model, load_model
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Activation
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Activation, BatchNormalization, Dropout
 from tensorflow.keras.optimizers import SGD
 
 import os
@@ -33,29 +33,39 @@ def vgg_11():
 	
 	model.add( Conv2D(64, kernel_size = (3,3), padding = 'same', input_shape=(32,32,3) ) )
 	model.add( Activation('relu') )
+	model.add(BatchNormalization())
 
 	model.add( Conv2D(64, kernel_size = (3,3), padding = 'same' ) )
 	model.add( Activation('relu') )
+	model.add(BatchNormalization())
+	model.add(Dropout(0.4))
 	model.add( MaxPool2D(pool_size=(2,2),strides=(2,2)) )
-	# model.add( MaxPool2D(pool_size=(3, 3), strides = 1 ) )
+	# model.add( MaxPool2D(pool_size=(3, 3), strides = (1,1) ) )
 
 	model.add( Conv2D(128, kernel_size = (3,3), padding = 'same' ) )
 	model.add( Activation('relu') )
+	model.add(BatchNormalization())
 
 	model.add( Conv2D(128, kernel_size = (3,3), padding = 'same' ) )
 	model.add( Activation('relu') )
+	model.add(BatchNormalization())
+	model.add(Dropout(0.4))
 	model.add( MaxPool2D(pool_size=(2,2),strides=(2,2)) )
-	# model.add( MaxPool2D(pool_size=(3, 3), strides = 1 ) )
+	# model.add( MaxPool2D(pool_size=(3, 3), strides = (1,1) ) )
 
 	model.add( Conv2D(256, kernel_size = (3,3), padding = 'same' ) )
 	model.add( Activation('relu') )
+	model.add(BatchNormalization())
 
 	model.add( Conv2D(256, kernel_size = (3,3), padding = 'same' ) )
 	model.add( Activation('relu') )
+	model.add(BatchNormalization())
+	model.add(Dropout(0.4))
 	model.add( MaxPool2D(pool_size=(2,2),strides=(2,2)) )
-	# model.add( MaxPool2D(pool_size=(3, 3), strides = 1 ) )
+	# model.add( MaxPool2D(pool_size=(3, 3), strides = (1,1) ) )
 
 	model.add( Conv2D(512, kernel_size = (3,3), padding = 'same' ) )
+	model.add(BatchNormalization())
 	model.add( Activation('relu') )
 
 	model.add( Conv2D(512, kernel_size = (3,3), padding = 'same' ) )
@@ -70,6 +80,101 @@ def vgg_11():
 
 	return model
 
+def vgg_11_02():
+	'''
+	conv-64  -> conv-64 + maxpool  -> conv-128        -> conv-128 + maxpool -> 
+	conv-512        -> conv-512    ->
+	FC-200   ->       FC-100       -> FC-10 + softmax
+	'''
+	
+	model = Sequential()
+	
+	model.add( Conv2D(64, kernel_size = (3,3), padding = 'same', input_shape=(32,32,3) ) )
+	model.add( Activation('relu') )
+	model.add(BatchNormalization())
+
+	model.add( Conv2D(64, kernel_size = (3,3), padding = 'same' ) )
+	model.add( Activation('relu') )
+	model.add(BatchNormalization())
+	model.add(Dropout(0.4))
+	model.add( MaxPool2D(pool_size=(2,2),strides=(2,2)) )
+	# model.add( MaxPool2D(pool_size=(3, 3), strides = (1,1) ) )
+
+	model.add( Conv2D(128, kernel_size = (3,3), padding = 'same' ) )
+	model.add( Activation('relu') )
+	model.add(BatchNormalization())
+
+	model.add( Conv2D(128, kernel_size = (3,3), padding = 'same' ) )
+	model.add( Activation('relu') )
+	model.add(BatchNormalization())
+	model.add(Dropout(0.4))
+	model.add( MaxPool2D(pool_size=(2,2),strides=(2,2)) )
+	# model.add( MaxPool2D(pool_size=(3, 3), strides = (1,1) ) )
+
+	model.add( Conv2D(512, kernel_size = (3,3), padding = 'same' ) )
+	model.add(BatchNormalization())
+	model.add( Activation('relu') )
+
+	model.add( Conv2D(512, kernel_size = (3,3), padding = 'same' ) )
+	model.add( Activation('relu') )
+
+	model.add( Flatten() )
+	model.add( Dense(200, activation='relu') )
+
+	model.add( Dense(100, activation='relu') )
+
+	model.add( Dense(10, activation='softmax') )
+
+	return model
+
+def vgg_11_03():
+	'''
+	conv-64  -> conv-64 + maxpool  ->
+	conv-256 -> conv-256 + maxpool -> conv-512        -> conv-512           ->
+	FC-200   ->       FC-100       -> FC-10 + softmax
+	'''
+	
+	model = Sequential()
+	
+	model.add( Conv2D(64, kernel_size = (3,3), padding = 'same', input_shape=(32,32,3) ) )
+	model.add( Activation('relu') )
+	model.add(BatchNormalization())
+
+	model.add( Conv2D(64, kernel_size = (3,3), padding = 'same' ) )
+	model.add( Activation('relu') )
+	model.add(BatchNormalization())
+	model.add(Dropout(0.4))
+	model.add( MaxPool2D(pool_size=(2,2),strides=(2,2)) )
+	# model.add( MaxPool2D(pool_size=(3, 3), strides = (1,1) ) )
+
+	model.add( Conv2D(256, kernel_size = (3,3), padding = 'same' ) )
+	model.add( Activation('relu') )
+	model.add(BatchNormalization())
+
+	model.add( Conv2D(256, kernel_size = (3,3), padding = 'same' ) )
+	model.add( Activation('relu') )
+	model.add(BatchNormalization())
+	model.add(Dropout(0.4))
+	model.add( MaxPool2D(pool_size=(2,2),strides=(2,2)) )
+	# model.add( MaxPool2D(pool_size=(3, 3), strides = (1,1) ) )
+
+	model.add( Conv2D(512, kernel_size = (3,3), padding = 'same' ) )
+	model.add(BatchNormalization())
+	model.add( Activation('relu') )
+
+	model.add( Conv2D(512, kernel_size = (3,3), padding = 'same' ) )
+	model.add( Activation('relu') )
+
+	model.add( Flatten() )
+	model.add( Dense(200, activation='relu') )
+
+	model.add( Dense(100, activation='relu') )
+
+	model.add( Dense(10, activation='softmax') )
+
+	return model
+
+
 def trainModel(model_name, train_data, train_labels, validation_data, validation_labels, epochs = 5, learning_rate = 0.01):
 	model = None
 	h5_storage_path = models_dir + "/" + model_name + "_" + str(learning_rate) + ".h5"
@@ -77,6 +182,10 @@ def trainModel(model_name, train_data, train_labels, validation_data, validation
 	
 	if model_name == "vgg_11":
 		model = vgg_11()
+	elif model_name == "vgg_11_02":
+		model = vgg_11_02()
+	elif model_name == "vgg_11_03":
+		model = vgg_11_03()
 
 	if model != None:
 		print("Start training model: " + model_name)
